67dd86607dbca687cebac73171279b388d070a92
Hasanmahmud000 <163021899+Hasanmahmud000@users.noreply.github.com>
2024-05-14 10:35:43 +0600

    Create BDStreamHub.js

---
+++ BDStreamHub.js
@@ -0,0 +1,32 @@
+#EXTM3U
+
+
+
+
+
+#EXTINF:-1 group-title="LIVE NOW" tvg-logo="https://i.postimg.cc/sDRYCTmH/20240321-192447.png",IPL 2024 Live ENG FHD
+http://starshare.live:8080/live/35858547/86368536/18452.ts
+
+#EXTINF:-1 group-title="LIVE NOW" tvg-logo="https://i.postimg.cc/sDRYCTmH/20240321-192447.png",IPL 2024 Live ENG HD
+http://starshare.live:8080/live/35858547/86368536/130714.ts
+
+#EXTINF:-1 group-title="LIVE NOW" tvg-logo="https://i.postimg.cc/sDRYCTmH/20240321-192447.png",IPL 2024 Live Toffee 
+https://its-nayeemparvez.xyz/toffee/stream.php?id=match-1&hls=master_1000.m3u8
+
+#EXTINF:-1 group-title="LIVE NOW" tvg-logo="https://i.postimg.cc/sDRYCTmH/20240321-192447.png",IPL 2024 Live ENG
+http://starshare.live:8080/live/35858547/86368536/304279.ts
+
+#EXTINF:-1 group-title="LIVE NOW" tvg-logo="https://i.postimg.cc/sDRYCTmH/20240321-192447.png",IPL 2024 Live ENG
+http://starshare.live:8080/live/35858547/86368536/2494.ts
+
+#EXTINF:-1 group-title="LIVE NOW" tvg-logo="https://i.postimg.cc/sDRYCTmH/20240321-192447.png",IPL 2024 Live HINDI
+http://starshare.live:8080/live/35858547/86368536/304278.ts
+
+#EXTINF:-1 group-title="LIVE NOW" tvg-logo="https://i.postimg.cc/sDRYCTmH/20240321-192447.png",IPL 2024 BANGLA (LIVE ONLY MATCH TIME)
+http://starshare.live:8080/live/35858547/86368536/304296.ts
+
+
+
+
+#EXTINF:-1 group-title="Notice" tvg-logo="https://i.imgur.com/n3r7sEr.jpg",Notice
+https://i.imgur.com/bpOkjlP.mp4
