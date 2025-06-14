027a22f65460cd7f85af261b052f920c790dfd19
Hasan Mahmud <163021899+Hasanmahmud000@users.noreply.github.com>
2025-06-14 10:30:25 +0600

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("bdstream-cache").then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json",
+        "./icon-192.png",
+        "./icon-512.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
